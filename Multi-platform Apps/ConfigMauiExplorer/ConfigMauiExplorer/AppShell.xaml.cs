55b75afe6114cc15416e368c188ba15d1c6b7848
Vanner <mathias.vanner@live.dk>
2025-11-11 19:09:30 +0100

    Afsted

---
+++ Multi-platform Apps/ConfigMauiExplorer/ConfigMauiExplorer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ConfigMauiExplorer;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Multi-platform Apps/ConfigMauiExplorer/ConfigMauiExplorer/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using ConfigMauiExplorer.Interfaces;
+using ConfigMauiExplorer.Services;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigMauiExplorer;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        builder.Services.AddSingleton<IFolderPicker, FolderPicker>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
