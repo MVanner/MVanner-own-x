03b5533d529678156900f0110ba2ef48eb7f984f
Vanner <mathias.vanner@live.dk>
2025-11-16 23:55:06 +0100

    Charactor New

--- Games/Pong/game/src/character.lua
+++ Games/Pong/game/src/character.lua
@@ -6,5 +6,9 @@ function Character:new()
 	char.name = "Unnamed"
 	char.sprite = function(sprite) return love.graphics.newImage(sprite) end
 	char.position = {x = 0, y = 0}
+	char.size = {width = 0, height = 0}
+	char.body = function (world, type) return love.physics.newBody(world, char.position.x, char.position.y, type) end
+	char.shape = function() return love.physics.newRectangleShape(char.size.width / 2, char.size.height / 2, char.size.width, char.size.height) end
+	char.fixture = function() return love.physics.newFixture(char.body(), char.shape()) end
 	return char	
 end
